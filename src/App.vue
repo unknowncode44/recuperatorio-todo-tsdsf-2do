6b0857501ed498bb896b608d5cbc0b44aae3a741
unknowncode44 <80708560+unknowncode44@users.noreply.github.com>
2024-10-28 17:01:14 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="img min-h-screen flex flex-col items-center">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.img {
+  /*background: url('https://github.com/cloworm/todo/blob/master/public/images/bg-desktop-dark.jpg?raw=true');*/
+  background-image: url('https://github.com/cloworm/todo/blob/master/public/images/bg-desktop-light.jpg?raw=true');
+  background-position: top;
+  background-repeat: no-repeat;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex-1 lg:w-2/3 xl:w-2/5 w-full px-7">
+    <div>
+      <p
+        class="text-3xl lg:text-4xl text-white font-bold tracking-widest pt-10 sm:pt-16 lg:pt-24 lg:pt-20 pb-6 lg:pb-10"
+      >
+        TODO
+      </p>
+    </div>
+  </div>
+</template>
